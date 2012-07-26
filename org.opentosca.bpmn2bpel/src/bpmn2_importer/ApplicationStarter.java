58f76e3235daeb42b0d472a4675b54bfb0997320
Oliver Kopp <koppdev@googlemail.com>
2012-07-26 11:03:49 +0200

    changes output path to "results" in the repository. Uses existing file

--- org.opentosca.bpmn2bpel/src/bpmn2_importer/ApplicationStarter.java
+++ org.opentosca.bpmn2bpel/src/bpmn2_importer/ApplicationStarter.java
@@ -21,46 +21,46 @@ import org.jbpt.graph.algo.rpst.RPST;
 import org.jbpt.graph.algo.rpst.RPSTNode;
 
 public class ApplicationStarter implements IApplication {
-	
+
 	@SuppressWarnings({"rawtypes", "unchecked"})
 	@Override
 	public Object start(IApplicationContext context) throws Exception {
-		
+
 		ResourceSet resourceSet = new ResourceSetImpl();
-		Bpmn2Resource res = (Bpmn2Resource) resourceSet.getResource(URI.createFileURI("C:/git-repositories/BPMN2BPEL/examples/Test-Bonds-wo-Gateways.xml"), true);
+		Bpmn2Resource res = (Bpmn2Resource) resourceSet.getResource(URI.createFileURI("C:/git-repositories/BPMN2BPEL/examples/Quasi3(flow).xml"), true);
 		DocumentRoot root = (DocumentRoot) res.getContents().get(0);
 		BPMNProcessTree wt = new BPMNProcessTree(res);
 		BPMNProcessTree rpstWt;
 		WFNode wftreeRoot;
-		
+
 		for (EObject content : root.eContents()) {
 			if (content instanceof Definitions) {
 				Definitions def = (Definitions) content;
 				System.out.println("id: " + def.getId());
 				System.out.println("class: " + def.getClass());
-				
+
 				wt.FillTree(content);
 				System.out.println(wt.toString());
 				wt.AssignImports(def.getImports());
 			} else {
-				
+
 				System.out.println(content.getClass());
 				System.out.println("class: " + content.getClass());
-				
+
 			}
 		}
-		
+
 		// Transform the BPMN Process Tree into a single entry Single Exit
 		// process tree
 		wt.transfrom2SESE();
-		
+
 		RPST rpstgraph = new RPST(wt);
 		wt.setRPST(rpstgraph);
 		RPSTNode rpstRoot = rpstgraph.getRoot();
 		// System.out.println(rpstgraph.getRoot().getFragment());
 		System.out.println(rpstgraph.toString());
 		System.out.println(rpstgraph.countVertices());
-		
+
 		// Restructure any quasicomponents this graph may have
 		wt.restructureQuasi(rpstgraph, rpstRoot, null, null, false);
 		rpstgraph = new RPST(wt);
@@ -68,31 +68,31 @@ public class ApplicationStarter implements IApplication {
 		rpstRoot = rpstgraph.getRoot();
 		// System.out.println(wt.toString());
 		System.out.println(rpstgraph.toString());
-		
+
 		// Traverse the workflowtree and create BpelModel
 		ProcessImpl bpelmodel = wt.BpmnProctree2BpelModel(rpstRoot);
-		
+
 		// The BPELResource is declared with the bpel model as its content
 		BPELResourceImpl resbpel = new BPELResourceImpl();
 		resbpel.getContents().add(bpelmodel);
-		
+
 		// The BPELWriter is created
 		BPELWriter writer = new BPELWriter();
 		Map<?, ?> p = new HashMap();
-		
+
 		// The path of the translated bpel is selected
-		File fstream = new File("C:/Users/admin/Documents/Tesis/Projekt/Workspaces/Bpmn2Bpel/Result-bpel.xml");
+		File fstream = new File("C:/git-repositories/BPMN2BPEL/results/Result-bpel.xml");
 		FileOutputStream fout = new FileOutputStream(fstream);
 		writer.write(resbpel, fout, p);
 		fout.close();
-		
+
 		return null;
 	}
-	
+
 	@Override
 	public void stop() {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 }
