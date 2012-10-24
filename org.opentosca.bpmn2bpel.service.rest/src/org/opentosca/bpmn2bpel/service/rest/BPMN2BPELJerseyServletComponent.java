f192648d70081ab0fcece40924e81be4d117964f
Oliver Kopp <kopp@iaas.uni-stuttgart.de>
2012-10-24 22:05:22 +0200

    Fixes run configuration and adds required jersey class

--- org.opentosca.bpmn2bpel.service.rest/src/org/opentosca/bpmn2bpel/service/rest/BPMN2BPELJerseyServletComponent.java
+++ org.opentosca.bpmn2bpel.service.rest/src/org/opentosca/bpmn2bpel/service/rest/BPMN2BPELJerseyServletComponent.java
@@ -1,18 +1,18 @@
 package org.opentosca.bpmn2bpel.service.rest;
 
 import org.osgi.service.http.HttpService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.sun.jersey.core.spi.component.ComponentContext;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
 
 public class BPMN2BPELJerseyServletComponent {
 	
-	// private static final Logger LOG =
-	// LoggerFactory.getLogger(BPMN2BPELJerseyServletComponent.class);
-
+	private static final Logger LOG = LoggerFactory.getLogger(BPMN2BPELJerseyServletComponent.class);
+	
+	
 	protected void bindHttpService(HttpService httpService) {
-		// BPMN2BPELJerseyServletComponent.LOG.debug("Binding HTTP Service");
+		BPMN2BPELJerseyServletComponent.LOG.debug("Binding HTTP Service");
 		try {
 			httpService.registerServlet("/bpmn2bpel", new com.sun.jersey.spi.container.servlet.ServletContainer(new BPMN2BPELApplication()), null, null);
 		} catch (Exception ex) {
@@ -21,7 +21,7 @@ public class BPMN2BPELJerseyServletComponent {
 	}
 	
 	protected void unbindHttpService(HttpService httpService) {
-		// BPMN2BPELJerseyServletComponent.LOG.debug("Unbinding HTTP Service");
+		BPMN2BPELJerseyServletComponent.LOG.debug("Unbinding HTTP Service");
 		httpService.unregister("/bpmn2bpel");
 	}
 	
